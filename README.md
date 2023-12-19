3845188322ef010aeea0c83b6ad06e29928f965f
bmqy <bmqy@qq.com>
2023-12-19 16:06:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# auto-type-confirm-text
+自动输入需要二次确认的文本。
+
+暂支持以下站点，删除项目时的输入项目名的二次确认。
+
+## 支持站点
+- [x] Github
+- [x] Gitee
+- [x] codeup.aliyun.com
+
+如需支持其它站点，可反馈，将酌情添加。
+## 更新日志
+### v1.0.0
+- 发布上线
+
+## 联系作者
+[@chanel](https://t.me/tcbmqy)
+
+[@group](https://t.me/tgbmqy)
---
+++ src/main.js
@@ -0,0 +1,88 @@
+const app = {
+    host: location.host,
+    init(){
+        console.log('init', this.host)
+        this.onMutationObserver();
+    },
+
+    dispatchInputEmit: function (element, isReact) {
+        let event = new Event('input', { bubbles: true });
+        if(isReact){
+            event.simulated = true;
+            let tracker = element._valueTracker;
+            if (tracker) {
+                tracker.setValue(element);
+            }
+        }
+        element.dispatchEvent(event);
+    },
+
+    onMutationObserver(){
+        let that = this;
+        let mos = new MutationObserver(function(mutations, observer) {
+            for (let mutation in mutations) {
+                let element = mutations[mutation];
+                // github
+                if(that.host === 'github.com'){
+                    if(element.target.id === 'repo-delete-warning-container'){
+                        let $verificationField = document.querySelector('#verification_field');
+                        if($verificationField){
+                            $verificationField.value = $verificationField.getAttribute('data-repo-nwo');
+                            document.querySelector('#repo-delete-proceed-button').disabled = false;
+                            that.dispatchInputEmit($verificationField, true)
+                        }
+                    }
+                }
+                // gitee
+                if(that.host === 'gitee.com'){
+                    if(element.target && element.target.classList.contains("fade")){
+                        let $pathWithNamespace = document.querySelector('#path_with_namespace');
+                        if($pathWithNamespace){
+                            $pathWithNamespace.value = document.querySelector('.highlight-black').innerText;
+                            that.dispatchInputEmit($pathWithNamespace)
+                        }
+                    }
+                }
+                // codeup.aliyun.com
+                if(that.host === 'codeup.aliyun.com'){
+                    if(element.target.classList.contains('next-overlay-inner')){
+                        let $nextOverlayWrapper = document.querySelector('.next-overlay-wrapper');
+                        let $deletePathResourceName = $nextOverlayWrapper.querySelector('.delete-path-resource-name');
+                        let $name = $nextOverlayWrapper.querySelector('#name');
+                        let $reason = $nextOverlayWrapper.querySelector('#reason');
+                        if($name){
+                            let $targetName = $deletePathResourceName.querySelector('label');
+                            $name.value = $targetName.innerText.split(' ')[1];
+                            that.dispatchInputEmit($name, true);
+                            $reason.value = '确认删除';
+                            that.dispatchInputEmit($reason, true);
+                        }
+                    }
+                }
+            }
+        });
+        // github
+        if(that.host === 'github.com'){
+            mos.observe(document.querySelector('#repo-delete-menu-dialog'), {
+                childList: true,
+                subtree: true,
+            });
+        }
+        // gitee
+        if(that.host === 'gitee.com'){
+            mos.observe(document.querySelector('.ui.dimmer.modals.page'), {
+                childList: true,
+                subtree: true,
+            });
+        }
+        // codeup.aliyun.com
+        if(that.host === 'codeup.aliyun.com'){
+            mos.observe(document.querySelector('body'), {
+                childList: true,
+                subtree: true,
+            });
+        }
+    }
+}
+
+app.init()
