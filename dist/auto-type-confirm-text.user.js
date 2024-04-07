503a6fa32ccfda14ff2165839d3715d6bfd97f24
bmqy <qq88268459@126.com>
2024-04-07 01:25:37 +0000

    打包更新

--- dist/auto-type-confirm-text.user.js
+++ dist/auto-type-confirm-text.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         自动输入二次确认文本
 // @namespace    npm/vite-plugin-monkey
-// @version      1.0.2
+// @version      1.0.3
 // @author       monkey
 // @description  自动输入需要二次确认的文本
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAAXNSR0IArs4c6QAAAcJJREFUeF7tWttxwjAQXDohnZBOoBJCJaSTpBPoBHIZG2SPjM6nEyNbqx8zg3QjrXfvYd0GjY9N4+cHASADGkeAEmicAHSClEDFEtgBOAKQ55zxC+D0t06eyVEzAy4AtskTxCfI4T81a9cKwBXAx9IB2HcSmMuC1Uhg6gXeRn9ksThrsYZiBeYQADJgiEAWi7MWF6C3xmQxCUjCcc6IvZrNa+dIGOu9ufwORwoAiRqSQEkUiQ2xd+gTpZABP4asS3sg67yvLqubA4C8xKnD93YeeUIIwBhZ66Y915UCQPb4f/aaAfjuqDoGVCMBYUGqhkgCUKuDTAEwK4F6xQAC4ClmR1tkgDETjAJHCQRoWqnlyG6VKes+yYCYdCgBSuCJgFVbKuE6TrLukz6APmCIAGuBVDXIWsDRcXmaohNkLRBxZgqKMQwyDDIMDhBgHsA8oOLP4iVvhnodJCWgiCzFp7z1YqSlq7FHD1GY71u7skrRQG6GpNvrbZejpQ7ibddaC0T3UWvF9wo0AmAshsiAGAKUgLeHcrRnjUqraZRkr3BGwxZ7hdfQLu/oTqZNLTEKuAJDAFzhXKAxMmCBL811y80z4A7Z+otB27V0OwAAAABJRU5ErkJggg==
-// @match        https://github.com/*/settings
-// @match        https://gitee.com/*/settings*
-// @match        https://codeup.aliyun.com/*/settings*
-// @match        https://vercel.com/*/settings*
-// @match        https://dash.cloudflare.com/*/workers/services/view/*/production/manage
+// @match        https://github.com/*
+// @match        https://gitee.com/*
+// @match        https://codeup.aliyun.com/*
+// @match        https://vercel.com/*
+// @match        https://dash.cloudflare.com/*
 // ==/UserScript==
 
 (function () {
