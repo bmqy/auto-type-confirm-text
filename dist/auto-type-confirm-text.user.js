4e54e542bbee27e1185bc104c9b4710b62293633
bmqy <qq88268459@126.com>
2025-11-13 08:21:59 +0000

    打包更新

--- dist/auto-type-confirm-text.user.js
+++ dist/auto-type-confirm-text.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动输入二次确认文本
 // @namespace    npm/vite-plugin-monkey
-// @version      1.0.8
+// @version      1.0.9
 // @author       bmqy
 // @description  自动输入需要二次确认的文本
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAAXNSR0IArs4c6QAAAcJJREFUeF7tWttxwjAQXDohnZBOoBJCJaSTpBPoBHIZG2SPjM6nEyNbqx8zg3QjrXfvYd0GjY9N4+cHASADGkeAEmicAHSClEDFEtgBOAKQ55zxC+D0t06eyVEzAy4AtskTxCfI4T81a9cKwBXAx9IB2HcSmMuC1Uhg6gXeRn9ksThrsYZiBeYQADJgiEAWi7MWF6C3xmQxCUjCcc6IvZrNa+dIGOu9ufwORwoAiRqSQEkUiQ2xd+gTpZABP4asS3sg67yvLqubA4C8xKnD93YeeUIIwBhZ66Y915UCQPb4f/aaAfjuqDoGVCMBYUGqhkgCUKuDTAEwK4F6xQAC4ClmR1tkgDETjAJHCQRoWqnlyG6VKes+yYCYdCgBSuCJgFVbKuE6TrLukz6APmCIAGuBVDXIWsDRcXmaohNkLRBxZgqKMQwyDDIMDhBgHsA8oOLP4iVvhnodJCWgiCzFp7z1YqSlq7FHD1GY71u7skrRQG6GpNvrbZejpQ7ibddaC0T3UWvF9wo0AmAshsiAGAKUgLeHcrRnjUqraZRkr3BGwxZ7hdfQLu/oTqZNLTEKuAJDAFzhXKAxMmCBL811y80z4A7Z+otB27V0OwAAAABJRU5ErkJggg==
@@ -114,13 +114,24 @@
               }
             }
           }
-          if (that.pathname === "/websites") {
+          if (["/websites", "/apps/installed", "/databases/mysql"].includes(that.pathname)) {
             if (element.target.querySelector(".el-dialog")) {
               if (element.target.querySelector(".el-dialog__title").innerText.indexOf("删除") > -1) {
                 let $dialog = element.target.querySelector(".el-dialog");
                 let $input = $dialog.querySelector(".el-input__inner[type=text]");
                 if ($input) {
                   $input.value = $input.getAttribute("placeholder");
+                  that.dispatchInputEmit($input, true);
+                }
+              }
+            }
+          } else if (["/containers/setting"].includes(that.pathname)) {
+            if (element.target.querySelector(".el-dialog")) {
+              if (element.target.querySelector(".card-header").innerText.indexOf("配置修改") > -1) {
+                let $dialog = element.target.querySelector(".el-dialog");
+                let $input = $dialog.querySelector(".el-input__inner[type=text]");
+                if ($input) {
+                  $input.value = "立即重启";
                   that.dispatchInputEmit($input, true);
                 }
               }
